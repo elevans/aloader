31b179434ab923ae40773b73a15be74c7391ceda
Edward Evans <elevans2@wisc.edu>
2022-07-29 13:35:48 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="aloader",
+    version="0.1.0.dev0",
+    author="Edward Evans",
+    author_email="elevans2@wisc.edu",
+    platforms=["any"],
+    description="Provides animations to running functions."
+)
